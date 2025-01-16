0786f07a7bc480e8ee86335780d4e38a0f94feee
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-01-16 22:34:26 +0600

    Updated

--- code_mama/mod_26/Consecutive Numbers.dart
+++ code_mama/mod_26/Consecutive Numbers.dart
@@ -4,7 +4,6 @@ bool canFormConsecutiveList(List<int> numbers) {
   if (numbers.isEmpty) {
     return true;
   }
-
   var uniqueNumbers = numbers.toSet();
   if (uniqueNumbers.length != numbers.length) {
     return false;
@@ -15,10 +14,11 @@ bool canFormConsecutiveList(List<int> numbers) {
 }
 
 void main() {
+  int n = int.parse(stdin.readLineSync()!);
+
   String? input = stdin.readLineSync();
   if (input != null) {
-    List<String> inputs = input.split(' ');
-    List<int> numbers = inputs.map(int.parse).toList();
+    List<int> numbers = input.split(' ').map(int.parse).toList();
 
     bool result = canFormConsecutiveList(numbers);
     print(result ? 'true' : 'false');
